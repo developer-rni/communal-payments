fb0898b1fa860c5bfa946eabeb2c34a4d0b8091d
developer-rni <developer.rni@gmail.com>
2020-11-10 10:56:53 +0300

    Build v1.7.3

--- recalculation_water.py
+++ recalculation_water.py
@@ -100,44 +100,59 @@ class Ui_Dialog(object):
         water_recalc = self.doubleSpinBox.value()
         self.sqlite_update_db(month_addr, year_addr, water_recalc)
         self.close()
-
+    
     def reject_data(self):
         self.close()
 
     def sqlite_update_db(self, month, year, water_rec):
         year_addr = str(year) + '_t13'
+        year_addr_next = str(year + 1) + '_t13'
 
         payment_type1 = 'water_meter'
         payment_type2 = 'water_difference'
         payment_type3 = 'water_change_text'
+        payment_type4 = 'water_to_pay'
+        payment_type5 = 'water_unit_price'
+        payment_type6 = 'total'
 
         if month == 'Январь':
             what_month = 'jan'
+            what_month_next = 'feb'
         elif month == 'Февраль':
             what_month = 'feb'
+            what_month_next = 'mar'
         elif month == 'Март':
             what_month = 'mar'
+            what_month_next = 'apr'
         elif month == 'Апрель':
             what_month = 'apr'
+            what_month_next = 'may'
         elif month == 'Май':
             what_month = 'may'
+            what_month_next = 'jun'
         elif month == 'Июнь':
             what_month = 'jun'
+            what_month_next = 'jul'
         elif month == 'Июль':
             what_month = 'jul'
+            what_month_next = 'aug'
         elif month == 'Август':
             what_month = 'aug'
+            what_month_next = 'sept'
         elif month == 'Сентябрь':
             what_month = 'sept'
+            what_month_next = 'oct'
         elif month == 'Октябрь':
             what_month = 'oct'
+            what_month_next = 'nov'
         elif month == 'Ноябрь':
             what_month = 'nov'
+            what_month_next = 'dec'
         else:
             what_month = 'dec'
+            what_month_next = 'jan'
         
         [real_water_meter], = MainMenu.cur.execute('SELECT {} FROM "{}" WHERE month="{}"'.format(payment_type1, year_addr, what_month))
-
 
         if real_water_meter != None and real_water_meter != 0:
 
@@ -150,6 +165,63 @@ class Ui_Dialog(object):
                 MainMenu.cur.execute('UPDATE "{}" SET {} = {} WHERE month="{}"'.format(year_addr, payment_type2, difference_new, what_month))
 
                 MainMenu.cur.execute('UPDATE "{}" SET {} = {} WHERE month="{}"'.format(year_addr, payment_type1, water_rec, what_month))
+
+                if what_month != 'dec':
+                    [difference_new_month], = MainMenu.cur.execute('SELECT {} FROM "{}" WHERE month="{}"'.format(payment_type2, year_addr, what_month_next))
+                    [water_to_pay_next_month], = MainMenu.cur.execute('SELECT {} FROM "{}" WHERE month="{}"'.format(payment_type4, year_addr, what_month_next))
+                    [total_next_month_old], = MainMenu.cur.execute('SELECT {} FROM "{}" WHERE month="{}"'.format(payment_type6, year_addr, what_month_next))
+                else:
+                    [difference_new_month], = MainMenu.cur.execute('SELECT {} FROM "{}" WHERE month="{}"'.format(payment_type2, year_addr_next, what_month_next))
+                    [water_to_pay_next_month], = MainMenu.cur.execute('SELECT {} FROM "{}" WHERE month="{}"'.format(payment_type4, year_addr_next, what_month_next))
+                    [total_next_month_old], = MainMenu.cur.execute('SELECT {} FROM "{}" WHERE month="{}"'.format(payment_type6, year_addr_next, what_month_next))
+
+                if water_to_pay_next_month != None and water_to_pay_next_month != 0:
+                    [water_unit_price], = MainMenu.cur.execute('SELECT {} FROM "{}" WHERE month="{}"'.format(payment_type5, year_addr, what_month))
+                    water_to_pay_new = difference_new * water_unit_price
+                    water_meter_new_month = int(water_rec) + difference_new_month
+                    total_next_month_new = (total_next_month_old - water_to_pay_next_month) + water_to_pay_new
+                    if what_month != 'dec':
+                        MainMenu.cur.execute('UPDATE "{}" SET {} = {} WHERE month="{}"'.format(year_addr, payment_type1, water_meter_new_month, what_month_next))
+                        MainMenu.cur.execute('UPDATE "{}" SET {} = {} WHERE month="{}"'.format(year_addr, payment_type4, water_to_pay_new, what_month_next))
+                        MainMenu.cur.execute('UPDATE "{}" SET {} = {} WHERE month="{}"'.format(year_addr, payment_type6, total_next_month_new, what_month_next))
+
+                        # /запись в общий отчет двух таблиц/
+                        # ---------------------------
+                        year_addr_t13 = year_addr
+                        year_addr_a61a = str(year) + '_a61a'
+                        [result_t13], = MainMenu.cur.execute('SELECT {} FROM "{}" WHERE month="{}"'.format(payment_type6, year_addr_t13, what_month_next))
+                        [result_a61a], = MainMenu.cur.execute('SELECT {} FROM "{}" WHERE month="{}"'.format(payment_type6, year_addr_a61a, what_month_next))
+                        if result_t13 is None:
+                            result_t13 = 0
+                        if result_a61a is None:
+                            result_a61a = 0
+                        general_total_result = result_t13 + result_a61a
+                        t_general_total_to_pay = 'general_total_to_pay'
+                        t_general_month = what_month_next + '_general_total'
+                        what_year = year
+                        MainMenu.cur.execute('UPDATE "{}" SET {} = {} WHERE year="{}"'.format(t_general_total_to_pay, t_general_month, general_total_result, what_year))
+                    else:
+                        MainMenu.cur.execute('UPDATE "{}" SET {} = {} WHERE month="{}"'.format(year_addr_next, payment_type1, water_meter_new_month, what_month_next))
+                        MainMenu.cur.execute('UPDATE "{}" SET {} = {} WHERE month="{}"'.format(year_addr_next, payment_type4, water_to_pay_new, what_month_next))
+                        MainMenu.cur.execute('UPDATE "{}" SET {} = {} WHERE month="{}"'.format(year_addr_next, payment_type6, total_next_month_new, what_month_next))
+
+                        # /запись в общий отчет двух таблиц/
+                        # ---------------------------
+                        year_addr_t13 = year_addr
+                        year_addr_a61a = str(year) + '_a61a'
+                        [result_t13], = MainMenu.cur.execute('SELECT {} FROM "{}" WHERE month="{}"'.format(payment_type6, year_addr_t13, what_month_next))
+                        [result_a61a], = MainMenu.cur.execute('SELECT {} FROM "{}" WHERE month="{}"'.format(payment_type6, year_addr_a61a, what_month_next))
+                        if result_t13 is None:
+                            result_t13 = 0
+                        if result_a61a is None:
+                            result_a61a = 0
+                        general_total_result = result_t13 + result_a61a
+                        t_general_total_to_pay = 'general_total_to_pay'
+                        t_general_month = what_month_next + '_general_total'
+                        what_year = year + 1
+                        MainMenu.cur.execute('UPDATE "{}" SET {} = {} WHERE year="{}"'.format(t_general_total_to_pay, t_general_month, general_total_result, what_year))
+
+
 
                 [water_change_text], = MainMenu.cur.execute('SELECT {} FROM "{}" WHERE month="{}"'.format(payment_type3, year_addr, what_month))
                 if water_change_text != None and water_change_text != ' *перерасчет':
